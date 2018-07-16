3501762b402556dab36ed6cfc3d3081e3cd5687a
Yifeng Wang <i@ewind.us>
2018-07-16 08:19:21 +0800

    update scripts

---
+++ scripts/new.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+const path = require('path')
+const fs = require('fs')
+const title = process.argv[2] || 'empty-new-post'
+const date = new Date().toISOString().slice(0, 10)
+
+const template = `---
+categories: Note
+
+tags:
+
+- Web
+
+date: ${date}
+
+toc: true
+
+title: ${title}
+---
+`
+
+const filePath = path.resolve('./source/_posts', title + '.md')
+fs.appendFileSync(filePath, template, 'utf8')
